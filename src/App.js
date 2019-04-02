8c9c176ec6ec61bb14047f7f2e71ec41326e964c
noy hadaria <noyhadaria@gmail.com>
2019-04-02 13:27:26 +0300

    fetch robots

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import Robot from './components/Robot'
 
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row } from 'reactstrap'
 class App extends Component {
+  state = {
+    robots: []
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => this.setState({ robots: data }))
+      .catch(err => console.log(err))
+  }
+
   render() {
+    const { robots } = this.state;
+    console.log(robots)
     return (
       <div>
         <h1 className="title text-center display-1">Robots App</h1>
         <Container>
           <Row>
-            <Col sm="3">
-              <Robot />
-            </Col>
+            {robots.map(robot => (
+              <Robot key={robot.id} robot={robot}/>
+            ))}
           </Row>
         </Container>
       </div>
--- src/components/Robot.js
+++ src/components/Robot.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import { Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
+import { Col, Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
 
-const Robot = () => {
+const Robot = ({ robot }) => {
   return (
-    <div className="Robot">
-        <Card body inverse color="danger">
-            <CardBody>
-                <CardTitle className="text-center font-weight-bold">Name</CardTitle>
-            </CardBody>
-            <img width="100%" src="https://robohash.org/1?size=200x200" alt="robot" />
-            <CardBody>
-                <CardSubtitle className="text-center">jfsj@jjf.com</CardSubtitle>
-            </CardBody>
-        </Card>
-    </div>
+    <Col sm="3">
+        <div className="Robot">
+            <Card body inverse color="danger">
+                <CardBody>
+                    <CardTitle className="text-center font-weight-bold">{ robot.name }</CardTitle>
+                </CardBody>
+                <img width="100%" src={`https://robohash.org/${robot.id}?size=200x200`} alt="robot" />
+                <CardBody>
+                    <CardSubtitle className="text-center">{ robot.email }</CardSubtitle>
+                </CardBody>
+            </Card>
+        </div>
+    </Col>
   )
 }
 
