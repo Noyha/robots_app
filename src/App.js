bea38974a7a90381c3c023c4973c587f216a78e6
noy hadaria <noyhadaria@gmail.com>
2019-04-03 18:14:23 +0300

    loading

--- src/App.js
+++ src/App.js
@@ -15,23 +15,27 @@ class App extends Component {
   }
 
   render() {
-    const { robots, search } = this.props.robot;
+    const { robots, search, loading } = this.props.robot;
     const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(search.toLowerCase()));
-    return (
-      <div className="text-center">
-        <h1 className="title font-weight-bold mt-5 mb-5 display-1">Robots App</h1>
-        <Container>
-          <Row className="mb-5">
-            <Col sm={{ size: 6, offset: 3 }}>
-              <SearchBar handelChange={ this.props.searchValue }/>
-            </Col>
-          </Row>
-          <Row className="mb-2">
-            <RobotList robots={ filteredRobots }/>
-          </Row>
-        </Container>
-      </div>
-    );
+    if (loading) {
+      return <h3>Loading...</h3>
+    } else {
+      return (
+        <div className="text-center">
+          <h1 className="title font-weight-bold mt-5 mb-5 display-1">Robots App</h1>
+          <Container>
+            <Row className="mb-5">
+              <Col sm={{ size: 6, offset: 3 }}>
+                <SearchBar handelChange={ this.props.searchValue }/>
+              </Col>
+            </Row>
+            <Row className="mb-2">
+              <RobotList robots={ filteredRobots }/>
+            </Row>
+          </Container>
+        </div>
+      );
+    }
   }
 }
 
